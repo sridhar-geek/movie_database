234cc884cfa332e94ff673cae9f5735df41abe8e
Sridhar <sridharmoturu@gmail.com>
2024-05-09 09:04:54 +0530

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
+// Imports from anthor files
+import Home from './Pages/Home.jsx'
+import Subscribe from './Pages/Subscribe'
+import NewsArctile from './Pages/NewsArctile'
+import Header from './Components/Header.jsx'
+import Footer from './Components/Footer.jsx'
 
-  return (
-    <div>
-
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='/subscribe' element={<Subscribe />} />
+      <Route path='/article/:id' element={<NewsArctile />} />
+    </Routes>
+    <Footer />
+  </div>
+)
 
 export default App
